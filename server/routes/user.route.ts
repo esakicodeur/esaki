7ab0b0e81d46e2fb94dd86fd50aadfb733d44971
Alex Esaki <apachecordovax@gmail.com>
2023-12-12 13:36:53 +0100

    update user info, password, avatar

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken } from "../controllers/user.controller";
+import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken, updatePassword, updateProfilePicture, updateUserInfo } from "../controllers/user.controller";
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 
 const userRouter = express.Router();
@@ -17,5 +17,11 @@ userRouter.get('/refresh', updateAccessToken);
 userRouter.get('/me', isAuthenticated, getUserInfo);
 
 userRouter.post('/social-auth', socialAuth);
+
+userRouter.put('/update-user-info', isAuthenticated, updateUserInfo);
+
+userRouter.put('/update-user-password', isAuthenticated, updatePassword);
+
+userRouter.put('/update-user-avatar', isAuthenticated, updateProfilePicture);
 
 export default userRouter;
