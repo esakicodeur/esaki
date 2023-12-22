fda2d9dc0564a187dc7e3153ee5945048be7b660
Alex Esaki <apachecordovax@gmail.com>
2023-12-22 17:37:22 +0100

    admin dashboard sidebar and create course UI

--- client/app/hooks/adminProtected.tsx
+++ client/app/hooks/adminProtected.tsx
@@ -9,7 +9,9 @@ interface ProtectedProps {
 export default function Protected({ children }: ProtectedProps) {
     const { user } = useSelector((state: any) => state.auth);
 
-    const isAdmin = user?.role === "admin";
+    if (user) {
+        const isAdmin = user?.role === "admin";
 
-    return isAdmin ? children : redirect("/");
+        return isAdmin ? children : redirect("/");
+    }
 }
