04659c03db69f47d6d5b54017a78b504fa8ec20e
Alex Esaki <apachecordovax@gmail.com>
2023-12-23 09:54:25 +0100

    create course feature

--- client/app/components/Admin/Course/CreateCourse.tsx
+++ client/app/components/Admin/Course/CreateCourse.tsx
@@ -1,14 +1,32 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CourseInformation from "./CourseInformation";
 import CourseOptions from "./CourseOptions";
 import CourseData from "./CourseData";
 import CourseContent from "./CourseContent";
 import CoursePreview from "./CoursePreview";
+import { useCreateCourseMutation } from '@/redux/features/courses/coursesApi';
+import toast from 'react-hot-toast';
+import { redirect } from 'next/navigation';
 
 type Props = {}
 
 const CreateCourse = (props: Props) => {
+    const [createCourse, {isLoading, isSuccess, error}] = useCreateCourseMutation();
+
+    useEffect(() => {
+        if (isSuccess) {
+            toast.success("Course create successfully !");
+            redirect("/admin/all-courses");
+        }
+        if (error) {
+            if ("data" in error) {
+                const errorMessage = error as any;
+                toast.error(errorMessage.data.message);
+            }
+        }
+    }, [isLoading, isSuccess, error]);
+
     const [active, setActive] = useState(0);
     const [courseInfo, setCourseInfo] = useState({
         name: "",
@@ -80,9 +98,11 @@ const CreateCourse = (props: Props) => {
 
     const handleCourseCreate = async (e: any) => {
         const data = courseData;
-    }
 
-    console.log(courseData)
+        if (!isLoading) {
+            await createCourse(data);        
+        }
+    }
 
     return (
         <div className='w-full flex min-h-screen'>
